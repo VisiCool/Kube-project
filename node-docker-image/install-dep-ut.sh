d56381b92c92676af2b1c3108a1c0f0e15f16310
Vishal Singh <68348103+VisiCool@users.noreply.github.com>
2024-08-13 19:54:17 +0530

    Update install-dep-ut.sh
    Remove the mistakes

--- node-docker-image/install-dep-ut.sh
+++ node-docker-image/install-dep-ut.sh
@@ -1,49 +1,48 @@
 #!/bin/bash
 
-#update the package index
+# Update the package index
 apt-get update
 
-#install basic dependencies and utilities
+# Install basic dependencies and utilities
 apt-get install -y \
-curl \
-wget \
-apt-transport-https \
-dnsutils \
-vim \
-gnupg \
-git \
-build-essential \
-net-tools \
-iputils-ping \
-unzip \
-zip \
-tar \
-bzip2 \
-gzip \
-software-properties-common\
-ca-certificates 
+    curl \
+    wget \
+    apt-transport-https \
+    dnsutils \
+    vim \
+    gnupg \
+    git \
+    build-essential \
+    net-tools \
+    iputils-ping \
+    unzip \
+    zip \
+    tar \
+    bzip2 \
+    gzip \
+    software-properties-common \
+    ca-certificates
 
-#
-# apt-get clean 
-# rm -rf /var/lib/apt/lists/*
-
-#Disable all swap space 
+# Disable all swap space 
 swapoff -a
 
-#Add Docker Official GPG key
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+# Create the directory for keyrings if it doesn't exist
+mkdir -p /etc/apt/keyrings
 
-#Make script executable 
+# Add Docker Official GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | tee /etc/apt/keyrings/docker.asc
+
+# Make script executable
 chmod a+r /etc/apt/keyrings/docker.asc
 
-#Add the repository to Apt sources:
+# Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
   $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
   tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-#update the package index
+# Update the package index
 apt-get update
- 
-#Install docker
-apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin 
+
+# Install Docker
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin
